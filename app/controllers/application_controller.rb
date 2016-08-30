fb74d36396876eeb4da173f99b3761ef99010143
Tyler Torola <tyler@tjt.codes>
2016-08-30 09:26:29 -0700

    adds basic rails auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	helper_method :current_user, :logged_in?
+
+	private
+
+	def current_user
+		return unless session[:token]
+		@current_user ||= User.find(session_token: session[:token])
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+
+	def login_user!(user)
+		session[:token] = user.reset_session_token!
+	end
+
+	def logout_user!
+		current_user.reset_session_token!
+		session[:token] = nil
+	end
 end
