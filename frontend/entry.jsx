dc745ac61d3d0e8baf2e16f4eb4a7932df0f60b6
Tyler Torola <tyler@tjt.codes>
2016-08-30 09:47:44 -0700

    adds entry.jsx file

---
+++ frontend/entry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Root from 'components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+	let rootEl = document.querySelector('#root');
+
+	// ReactDOM.render(<Root />, rootEl);
+});
